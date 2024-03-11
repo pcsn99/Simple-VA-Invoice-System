3cbd9e196c26002145c95b1ec657905a4b89fd48
User <User@DESKTOP-LOC4SSC>
2024-03-11 12:44:22 +0800

    added other methods for the invoice management, all thats left are the analytics

--- ITCC_mysql/src/activities/MainProgram.java
+++ ITCC_mysql/src/activities/MainProgram.java
@@ -6,6 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainProgram {
 	
@@ -339,15 +342,15 @@ public class MainProgram {
 	
 	
 	
-	private static void addInvoice(int client_id, String date ) {
+	private static void addInvoice(int client_id, java.sql.Date date ) {
 		try (Connection connection = getConnection()){
-			String insertQuery = "insert into invoice (client_id, status, price) values (?,?)" ;
+			String insertQuery = "insert into invoice (client_id, event_date) values (?,?)" ;
 			try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)){
 				preparedStatement.setInt(1,  client_id);
-				preparedStatement.setString(2, date);
+				preparedStatement.setDate(2, date);
 				int rowsAffected = preparedStatement.executeUpdate();
 				if (rowsAffected > 0) {
-					System.out.println("Service added");
+					System.out.println("Invoice added");
 				} else {
 					System.out.println("Failed to add Service");
 				}
@@ -367,7 +370,7 @@ public class MainProgram {
 						int id = resultSet.getInt("invoice_id");
 						String firstName = getClientFirstName(id);
 						String lastName = getClientLastName(id);
-						String date = resultSet.getString("date");
+						String date = resultSet.getString("event_date");
 						System.out.println(id + ", " + firstName + " " + lastName + ", " + date);
 					}
 				}
@@ -376,6 +379,173 @@ public class MainProgram {
 			e.printStackTrace();
 		}
 	}
+	
+	private static void displayInvoice(int client_id) {
+		try (Connection connection = getConnection()) {
+	        String query = "select * from invoice where client_id = ?";
+	        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+	            preparedStatement.setInt(1, client_id);
+
+	            try (ResultSet resultSet = preparedStatement.executeQuery()){
+					System.out.println("( Invoice ID, Date)");
+					while(resultSet.next()) {
+						int id = resultSet.getInt("invoice_id");
+						Date date = resultSet.getDate("event_date");
+						System.out.println(id + ", " + date);
+					}
+				}
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	private static void addServiceToInvoice(int invoice_id, int service_id, int service_amount) {
+		try (Connection connection = getConnection()){
+			String insertQuery = "insert into invoice_services (invoice_id, service_id, service_amount) values (?,?,?)" ;
+			try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)){
+				preparedStatement.setInt(1,  invoice_id);
+				preparedStatement.setInt(2, service_id);
+				preparedStatement.setFloat(3, service_amount);
+				int rowsAffected = preparedStatement.executeUpdate();
+				if (rowsAffected > 0) {
+					System.out.println("Service added to invoice");
+				} else {
+					System.out.println("Failed to add Service to invoice");
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	private static void updateInvoiceServiceAmount(int invoice_services_id, Float amount) {
+		try (Connection connection = getConnection()){
+			String query = "update invoice_services set service_amount = ? where invoice_services_id = ?";
+			try (PreparedStatement preparedStatement = connection.prepareStatement(query)){
+				preparedStatement.setFloat(1, amount);
+				preparedStatement.setInt(2, invoice_services_id);
+				int rowsAffected = preparedStatement.executeUpdate();
+				if (rowsAffected > 0) {
+					System.out.println("Service amount updated");
+				} else {
+					System.out.println("no data updated");
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}			
+	}
+	
+	private static void displayInvoiceServices(int invoice_id) {
+		try (Connection connection = getConnection()) {
+	        String query = "SELECT invoice_services.invoice_services_id, service.service_name, invoice_services.service_amount, service.price " +
+                   		   "FROM invoice_services  " +
+                   		   "JOIN service  ON invoice_services.service_id = service.service_id " +
+                   		   "JOIN invoice  ON invoice_services.invoice_id = invoice.invoice_id " +
+	        			   "WHERE invoice.invoice_id = ?";
+	        
+	        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+	            preparedStatement.setInt(1, invoice_id);
+
+	            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+	            	 System.out.println("Invoice Services:");
+	                    while (resultSet.next()) {
+	                        int invoiceId = resultSet.getInt("invoice_services_id");
+	                        float price = resultSet.getFloat("price");
+	                        String serviceName = resultSet.getString("service_name");
+	                        float serviceAmount = resultSet.getFloat("service_amount");
+
+	                        System.out.println("Invoice ID: " + invoiceId + ", Service Name: " + serviceName + ", Price per: " + price +
+	                                		   ", Service Amount: " + serviceAmount);
+	                    }
+	            }
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	private static void removeInvoiceService(int invoice_services_id) {
+		try(Connection connection = getConnection()){
+			String query = "delete from invoice_services where invoice_services_id =?";
+			try (PreparedStatement preparedStatement = connection.prepareStatement(query)){
+				preparedStatement.setInt(1, invoice_services_id);
+				int rowsAffected = preparedStatement.executeUpdate();
+				if (rowsAffected > 0) {
+					System.out.println("Service Removed");
+				} else {
+					System.out.println("no data removed");
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static float totalSpendingOfClient(int client_id) {
+		
+		 try (Connection connection = getConnection()) {
+		        String query = "select " +
+		                	   "sum(service.price * invoice_services.service_amount) as total_spending " +
+		                	   "from client_info " +
+		                	   "join invoice on client_info.client_id = invoice.client_id " +
+		                	   "join invoice_services on invoice.invoice_id = invoice_services.invoice_id " +
+		                	   "join service on service.service_id = invoice_services.service_id " +
+		                	   "where client_info.client_id = ?";
+
+		        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+		            preparedStatement.setInt(1, client_id);
+
+		            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+		                while (resultSet.next()) {
+		                    return resultSet.getFloat("total_spending");
+		                }
+		            }
+		        }
+		    } catch (SQLException e) {
+		        e.printStackTrace();
+		    }
+		    return 0;
+	}
+	
+	 private static float displayTotalSpending(int invoice_id) {
+	        try (Connection connection = getConnection()) {
+	            String query = "select " +
+	            			   "invoice.*, " +
+	            			   "sum(service.price * invoice_services.service_amount) as total_spending "+
+	            			   "from invoice "+
+	            			   "join invoice_services  ON invoice.invoice_id = invoice_services.invoice_id "+
+	            			   "join service  ON service.service_id = invoice_services.service_id "+
+	            			   "where invoice.invoice_id = ?;";
+
+	            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+		            preparedStatement.setInt(1, invoice_id);
+
+		            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+		            	 
+		                    while (resultSet.next()) {
+		                        float serviceAmount = resultSet.getFloat("total_spending");
+
+		                        return serviceAmount;
+		                    }
+		            }
+		        }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+			return 0;
+	    }
+	 
+	 private static Date parseDate(String dateString) {
+	        try {
+	            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	            return sdf.parse(dateString);
+	        } catch (ParseException e) {
+	            e.printStackTrace();
+	            return null;
+	        }
+	    }
 	
 	
 	
@@ -395,7 +565,8 @@ public class MainProgram {
 	
 	public static void main(String[] args) {
 		
-		displayAllInvoice();
+		
+		//displayAllInvoice();
 		//System.out.println(getClientFirstName(1));
 		//System.out.println(getClientLastName(1));
 		//int x = getClientID("Paul","Atreidis");
@@ -417,7 +588,13 @@ public class MainProgram {
 		//displayAllService();
 		
 		//displayService(1);
-		 
-		 
+		Date userDate = parseDate("2024-07-05");
+		addInvoice(1,new java.sql.Date(userDate.getTime()));
+		// displayAllInvoice();
+		// displayInvoice(1);
+		addServiceToInvoice(1,2,1);
+		//displayInvoiceServices(1);
+		System.out.println("Total Spending: "+ displayTotalSpending(1));
+		System.out.println(totalSpendingOfClient(1));
 		   }
 	}
